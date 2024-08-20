5261d10bd4cdd28489c3b4de146fdb2f8638e439
Kaviya <156312927+Kaviyamadeshvaran@users.noreply.github.com>
2024-08-20 20:46:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+const data = {
+    "id": 1,
+    "title": "Inception",
+    "director": "GVPrakash",
+    "year": 2010,
+    "poster": "Inception.jpeg"
+};
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('static'));
+
+app.get("/", (req, res) => {
+    res.render("forms.ejs", { data: data }); 
+});
+
+app.listen(port, () => {
+    console.log("Server is started on port " + port);
+});
